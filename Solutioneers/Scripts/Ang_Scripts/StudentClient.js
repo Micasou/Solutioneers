e0b6917f21e8d166acf259af486e7a6505c9d375
Tarik Merzouk <tarikmerzouk@yahoo.com>
2016-05-17 16:32:38 -0700

    Added test angular module and controller to the body of _Layout. Angular source in Source\Ang_Scripts

---
+++ Solutioneers/Scripts/Ang_Scripts/StudentClient.js
@@ -0,0 +1,7 @@
+﻿
+var StudentApp = angular.module("StudentApp", [])
+
+StudentApp.controller('StudentController', function ($scope) {
+    $scope.AppName = "Solutioneers";
+    $scope.name = "default name";
+});
